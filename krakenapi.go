3a427616b37b9e4157a343c8d801ce603231314d
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-02-05 22:30:54 -0800

    Able to check balance

--- krakenapi.go
+++ krakenapi.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-
 	"github.com/beldur/kraken-go-api-client"
 )
 
 func main() {
-	api := krakenapi.New("KEY", "SECRET")
+	api := krakenapi.New("BlNtopq1GpooZAqqofMF6pxy7shaGss+sJlw3P1XtLW7eUd10frJyOix", "nBBwYFp5wNMXQq1sCzMth4ySaiVyb94CkkGCchKftT+2fKpPI39YH55fp31L4NnvHEJ+7sPhTp79nOJLZHlgEQ==")
 	result, err := api.Query("Ticker", map[string]string{
 		"pair": "XXBTZEUR",
 	})
@@ -25,5 +24,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	balance, err := api.Balance ()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(ticker.XXBTZEUR.OpeningPrice)
+
+	fmt.Println(balance);
 }
